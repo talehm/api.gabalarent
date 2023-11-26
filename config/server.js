6f38e40bc765c8a54929a75d4dad2f9018e2c232
talehm <talehmuzaffer@gmail.com>
2023-11-26 21:42:22 +0100

    appkeys

--- config/server.js
+++ config/server.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+	host: env('HOST', '0.0.0.0'),
+	port: env.int('PORT', 1337),
+	app: {
+			keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+	},
+	webhooks: {
+			populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
 	},
 	settings: {
-		// ... other settings
-		session: {
-				// ... other session configurations
-				// Add the following line
-				keys: ['myKeyA', 'myKeyB'],
-		},
-},
+			// ... other settings
+			session: {
+					// ... other session configurations
+					// Add the following line
+					keys: env.array('SESSION_KEYS', ['myKeyA', 'myKeyB']),
+			},
+	},
 });
