40c1b244d95486e43c947b201bca4eb6c47d9161
talehm <talehmuzaffer@gmail.com>
2023-12-16 17:30:33 +0100

    middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,17 +6,8 @@ module.exports = [
 	config: {
 		enabled: true,
 		headers: '*',
-		origin: [
-			'http://127.0.0.1:1337',
-			'http://127.0.0.1:3000',
-			'http://127.0.0.1:9000',
-			'http://127.0.0.1:8081',
-			'https://www.gabalarent.az',
-			'https://gabalarent.az',
-			'https://api.gabalarent.az',
-		],
+		origin: '*',
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		credentials: true,
 	}
 	},
 	'strapi::poweredBy',
